bcfc20df2c29c661285ebd7bf4602b6cf45f5e12
GHKamilD <95308152+GHKamilD@users.noreply.github.com>
2024-04-30 12:23:41 +0200

    Removing irrelevant parties from tooltip

--- mapa_wojewodztw.py
+++ mapa_wojewodztw.py
@@ -5,15 +5,21 @@ import shapely
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
+def create_tooltip(row):
+    # Extract relevant data and sort it
+    party_data = {party: row[party] for party in ['PiS', 'KO', 'TD', 'Lewica', 'Konf', 'BS', 'OKS', 'SS']}
+    sorted_parties = sorted([(party, seats) for party, seats in party_data.items() if seats > 0], key=lambda x: x[1], reverse=True)
+    # Format the tooltip string
+    return '<br>'.join([f"{party}: {seats}" for party, seats in sorted_parties])
 
 wojewodztwa = geopandas.read_file('wojewodztwa-max.geojson')
 wojewodztwa = wojewodztwa.sort_values(by="nazwa")
 wojewodztwa = wojewodztwa.set_index('nazwa')
 dane = pandas.read_csv('nauka_map.csv', delimiter=";")
 wojewodztwa_z_danymi = wojewodztwa.merge(dane, on='nazwa')
-print(wojewodztwa_z_danymi['Konf'])
 wojewodztwa_z_danymi['color'] = wojewodztwa_z_danymi.apply(lambda row: 'orange' if row['Koalicja'] > row['Opozycja'] else ('blue' if row['Koalicja'] < row['Opozycja'] else 'grey'), axis=1)
-#print(merged_df['color'])
+wojewodztwa_z_danymi['tooltip'] = wojewodztwa_z_danymi.apply(create_tooltip, axis=1)
+print(wojewodztwa_z_danymi['tooltip'])
 """fig, ax = plt.subplots(figsize=(10, 8))
 wojewodztwa_z_danymi.plot(ax=ax, color=wojewodztwa_z_danymi['color'], edgecolor='black', legend=True)
 ax.set_axis_off()
@@ -35,7 +41,6 @@ m = folium.Map(location=[52, 19], zoom_start=6, zoom_control=False,
     scrollWheelZoom=False,
     dragging=False,)
 
-# Add GeoJSON data with correct coloring
 folium.GeoJson(
     wojewodztwa_z_danymi,
     style_function=lambda feature: {
@@ -45,9 +50,12 @@ folium.GeoJson(
         'fillOpacity': 0.5
     },
     highlight_function=lambda x: {'weight': 3, 'fillOpacity': 0.7},
-    tooltip=folium.GeoJsonTooltip(fields=['PiS', 'KO','TD','Lewica', 'Konf','BS', 'OKS'], aliases=['PiS', 'KO','TD','Lewica', 'Konf', 'BS', 'OKS'],
-    labels=True, sticky=True, localize= True),).add_to(m)
-m.save('map.html')
+    tooltip=folium.GeoJsonTooltip(fields=['tooltip'],
+        aliases=[''],
+        labels=True,
+        localize=True,
+        style=("font-size: 12px; font-weight: bold;")),).add_to(m)
+#m.save('map.html')
 m.show_in_browser()
 #merged_df.plot(cmap='RdYlBu', categorical=True, column='color', legend=True, legend_kwds={"loc": "center left", "bbox_to_anchor": (1, 0.5), "fmt": "{:.0f}"})
 #ax.legend(['orange', 'blue'], ['Koalicja', 'Opozycja'], loc='center')
