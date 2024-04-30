dcadad22abfb07224ac6581232055cf254f8b740
GHKamilD <95308152+GHKamilD@users.noreply.github.com>
2024-04-30 17:51:21 +0200

    Cleanup + some more

--- mapa_wojewodztw.py
+++ mapa_wojewodztw.py
@@ -6,10 +6,9 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
 def create_tooltip(row):
-    # Extract relevant data and sort it
-    party_data = {party: row[party] for party in ['PiS', 'KO', 'TD', 'Lewica', 'Konf', 'BS', 'OKS', 'SS']}
+    party_names = dane.columns[4:]
+    party_data = {party: row[party] for party in party_names}
     sorted_parties = sorted([(party, seats) for party, seats in party_data.items() if seats > 0], key=lambda x: x[1], reverse=True)
-    # Format the tooltip string
     return '<br>'.join([f"{party}: {seats}" for party, seats in sorted_parties])
 
 wojewodztwa = geopandas.read_file('wojewodztwa-max.geojson')
@@ -19,27 +18,10 @@ dane = pandas.read_csv('nauka_map.csv', delimiter=";")
 wojewodztwa_z_danymi = wojewodztwa.merge(dane, on='nazwa')
 wojewodztwa_z_danymi['color'] = wojewodztwa_z_danymi.apply(lambda row: 'orange' if row['Koalicja'] > row['Opozycja'] else ('blue' if row['Koalicja'] < row['Opozycja'] else 'grey'), axis=1)
 wojewodztwa_z_danymi['tooltip'] = wojewodztwa_z_danymi.apply(create_tooltip, axis=1)
-print(wojewodztwa_z_danymi['tooltip'])
-"""fig, ax = plt.subplots(figsize=(10, 8))
-wojewodztwa_z_danymi.plot(ax=ax, color=wojewodztwa_z_danymi['color'], edgecolor='black', legend=True)
-ax.set_axis_off()
-orange_patch = mpatches.Patch(color='orange', label='Koalicja')
-blue_patch = mpatches.Patch(color='blue', label='Opozycja')
-legend = ax.legend(handles=[orange_patch, blue_patch], loc='center left', bbox_to_anchor=(1.05, 0.5))"""
-"""m = wojewodztwa_z_danymi.explore(
-    column='color',
-    color = 'color',
-    tooltip=['nazwa'],
-    popup=True,
-    tiles='CartoDB positron',
-    cmap='Set1',
-    style_kwds=dict(color='black')
-)
-m.save('map.html')
-m"""
-m = folium.Map(location=[52, 19], zoom_start=6, zoom_control=False,
+m = folium.Map(location=[52, 19], zoom_start=6, zoom_control=False, min_zoom=6, max_zoom=6 ,
     scrollWheelZoom=False,
     dragging=False,)
+m.options['doubleClickZoom'] = False
 
 folium.GeoJson(
     wojewodztwa_z_danymi,
@@ -52,17 +34,8 @@ folium.GeoJson(
     highlight_function=lambda x: {'weight': 3, 'fillOpacity': 0.7},
     tooltip=folium.GeoJsonTooltip(fields=['tooltip'],
         aliases=[''],
-        labels=True,
+        labels=False,
         localize=True,
         style=("font-size: 12px; font-weight: bold;")),).add_to(m)
-#m.save('map.html')
+m.save('map.html')
 m.show_in_browser()
-#merged_df.plot(cmap='RdYlBu', categorical=True, column='color', legend=True, legend_kwds={"loc": "center left", "bbox_to_anchor": (1, 0.5), "fmt": "{:.0f}"})
-#ax.legend(['orange', 'blue'], ['Koalicja', 'Opozycja'], loc='center')
-"""fig, ax = plt.subplots()
-patches = ax.get_legend_handles_labels()
-
-wojewodztwa_z_danymi.plot(column='color', cmap='RdYlBu', ax=ax, legend=True)
-ax.set_title('Polish Voivodeships by Koalicja and Opozycja')"""
-#plt.show()
-#print(merged_df["Miejsca"])
